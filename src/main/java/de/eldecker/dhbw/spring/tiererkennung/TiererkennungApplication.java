f0d76e8cbec29a9185f21de0d90014b42378ace5
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-28 17:06:36 +0200

    Projekt frisch mit Spring Initializr erzeugt https://start.spring.io/#!type=maven-project&language=java&platformVersion=4.1.0&packaging=jar&configurationFileFormat=properties&jvmVersion=21&groupId=de.eldecker.dhbw.spring&artifactId=tiererkennung&packageName=de.eldecker.dhbw.spring.tiererkennung&dependencies=spring-ai-ollama

---
+++ src/main/java/de/eldecker/dhbw/spring/tiererkennung/TiererkennungApplication.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.tiererkennung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiererkennungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiererkennungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/tiererkennung/TiererkennungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.tiererkennung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiererkennungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
